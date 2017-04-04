a34e2dd07584f2670693c6283a8271d6fa2135e5
Justafigurehead <strachanmichaela@hotmail.com>
2017-04-04 16:14:50 +0100

    Finished server.js and added empty controllers index and animals.

--- server.js
+++ server.js
@@ -6,3 +6,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(require('./controllers'));
+
+app.use(express.static('client/build'));
+
+app.listen(3000, function(){
+  console.log("App is running at" + this.address().port);
+});
